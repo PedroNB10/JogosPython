b02793dd0ee944cc81364da25b42100001e300ec
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-09-17 19:25:05 -0300

    adicionei validações com palavras com espaços e letras maiusculas

--- forca.py
+++ forca.py
@@ -3,8 +3,11 @@ def mostra_letras_encontradas(tamanho_palavra,palavra):
     print("\n*********************************") 
     print("Resultados:\n")
     for i in range(tamanho_palavra):
-        if palavra[i] != "_":
+        if palavra[i] != "_" and palavra[i] != " ":
             print(palavra[i],end=' ' )
+            
+        elif palavra[i] == " ":
+            print(" ",end=' ')
         else:
             print("_ ", end=' ')
 
@@ -25,26 +28,37 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    palavra_auxiliar = list("______")
+
+    palavra_secreta = "Estados Unidos"
+    palavra_auxiliar = []
+    
+    for i in range(len(palavra_secreta)):
+        if palavra_secreta[i] != " ": 
+            palavra_auxiliar.append("_")
+        else:
+            palavra_auxiliar.append(" ")
+    
+    
     jogo_terminou = False
     jogador_ganhou = False
     contador_acertos = 0
     contador_erros = 0
     
+    mostrar_tentativas(contador_erros)
 
-    
+
+        
     while(not jogo_terminou):
         
         chute_letra = input("Digite a letra:")
+        chute_letra =  chute_letra.strip()
         
-        if (chute_letra in palavra_secreta) and (chute_letra not in palavra_auxiliar) :
+        if ((( chute_letra.upper() )in palavra_secreta) and (( chute_letra.upper() ) not in palavra_auxiliar) or (( chute_letra.lower() )in palavra_secreta) and (( chute_letra.lower() ) not in palavra_auxiliar)) and len(chute_letra) == 1:
             print("Você encontrou uma letra :D !")
-            index = palavra_secreta.find(chute_letra)
             j = 0
             for i in range(len(palavra_auxiliar)):
-                if palavra_secreta[i] == chute_letra:
-                    palavra_auxiliar[j] = chute_letra
+                if (palavra_secreta[i].lower() or palavra_secreta[i].upper() ) == chute_letra :
+                    palavra_auxiliar[j] = palavra_secreta[i]
                     contador_acertos = contador_acertos + 1
                 j = j + 1
             
@@ -61,7 +75,7 @@ def jogar():
 
             
         
-        if(contador_acertos == len(palavra_secreta)):
+        if contador_acertos == len(palavra_secreta.replace(" ","")):
             jogo_terminou = True
             jogador_ganhou = True
             
