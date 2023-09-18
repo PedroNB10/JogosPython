f2317c02b5748b8a5af9ad30d72cd1a5bcdde30c
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-09-18 10:52:48 -0300

    melhorias no projeto da forca

--- forca.py
+++ forca.py
@@ -1,35 +1,23 @@
 
-def mostra_letras_encontradas(tamanho_palavra,palavra):
-    print("\n*********************************") 
-    print("Resultados:\n")
-    for i in range(tamanho_palavra):
-        if palavra[i] != "_" and palavra[i] != " ":
-            print(palavra[i],end=' ' )
-            
-        elif palavra[i] == " ":
-            print(" ",end=' ')
-        else:
-            print("_ ", end=' ')
-
-    print("\n")
-    print("*********************************") 
-        
-def mostrar_tentativas(erros):
-    print("\n*********************************") 
-    print("Você tem {} tentativas restantes**".format(6 - erros))
-    print("*********************************\n") 
-    
-
-
-    
+import random
         
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    arquivo = open("palavras.txt","r")
+    palavras = []
+    
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
 
-    palavra_secreta = "Estados Unidos"
+    arquivo.close()
+    
+    numero = random.randrange(0,len(palavras) - 1) 
+    palavra_secreta  = palavras[numero]
+    
     palavra_auxiliar = []
     
     for i in range(len(palavra_secreta)):
@@ -51,7 +39,7 @@ def jogar():
     while(not jogo_terminou):
         
         chute_letra = input("Digite a letra:")
-        chute_letra =  chute_letra.strip()
+        chute_letra =  chute_letra.strip().lower()
         
         if ((( chute_letra.upper() )in palavra_secreta) and (( chute_letra.upper() ) not in palavra_auxiliar) or (( chute_letra.lower() )in palavra_secreta) and (( chute_letra.lower() ) not in palavra_auxiliar)) and len(chute_letra) == 1:
             print("Você encontrou uma letra :D !")
@@ -67,9 +55,10 @@ def jogar():
 
         else:
             contador_erros = contador_erros + 1
-            print("Não foi dessa vez!, essa letra não está presente na palavra")     
+            print("Não foi dessa vez!, essa letra não está presente na palavra") 
+            desenha_forca(contador_erros)    
                  
-        mostrar_tentativas(contador_erros)
+       # mostrar_tentativas(contador_erros)
         mostra_letras_encontradas(len(palavra_auxiliar),palavra_auxiliar)
         
 
@@ -88,10 +77,76 @@ def jogar():
         print("Parabéns você decifrou a palavra !!!")
     else:
         print("Não foi dessa vez, tente novamente :)")
+        print("A palavra era: {}".format(palavra_secreta))
 
 
-print("Fim do jogo")
+    print("Fim do jogo")
+    
+    
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
 
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+def mostra_letras_encontradas(tamanho_palavra,palavra):
+    print("\n*********************************") 
+    print("Resultados:\n")
+    for i in range(tamanho_palavra):
+        if palavra[i] != "_" and palavra[i] != " ":
+            print(palavra[i],end=' ' )
+            
+        elif palavra[i] == " ":
+            print(" ",end=' ')
+        else:
+            print("_ ", end=' ')
+
+    print("\n")
+    print("*********************************") 
+        
+def mostrar_tentativas(erros):
+    print("\n*********************************") 
+    print("Você tem {} tentativas restantes**".format(6 - erros))
+    print("*********************************\n") 
 
 
         
